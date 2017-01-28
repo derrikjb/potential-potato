d81d5fb476118908a11bc68667c9ad69c4a42fd8
derrikjb <derrikjb93@gmail.com>
2017-01-28 13:55:56 -0600

    Control.lua update

--- control.lua
+++ control.lua
@@ -1,23 +1,52 @@
 
---[[ Main file for reactor control*/ --]]
+--[[ Main file for reactor control --]]
 
 local filesystem = require("filesystem")
 local component = require("component")
 local keyboard = require("keyboard")
 local event = require("event")
 
-local reactor = isReactor()
+local reactor = component.br_reactor
 
+-- Check if connected to reactor computer control
 if (reactor)
   then
-    print("Error: Computer not connected to Reactor Computer Port")
-  else
     print("Successful connection to reactor")
+  else
+    print("Error: Computer not connected to Reactor Computer Port")
 end
 
+-- Check if reactor is a valid configuration
+if (reactor.getConnected())
+  then
+    print("Reactor is Valid")
+  else
+    print("Reactor structure is invalid")
+end
+
+reactor["stats"] = {}
+reactor.stats["state"] = false
+reactor.stats["fuelCurrent"] = 0
+reactor.stats["fuelMax"] = 0
+reactor.stats["wasteCurrent"] = 0
+reactor.stats["energyStored"] = 0
+reactor.stats["energyTick"] = 0
+reactor.stats["controlRodLevel"] = 0
+
+function pullReactorInfo(reactor)
+  reactor.stats.state = reactor.getActive()
+  reactor.stats.fuelCurrent = reactor.getFuelAmount()
+  reactor.stats.fuelMax = reactor.getFuelAmountMax()
+  reactor.stats.wasteCurrent = reactor.getWasteAmount()
+  reactor.stats.energyStored = reactor.getEnergyStored()
+  reactor.stats.energyTick = reactor.getEnergyProducedLastTick()
+  reactor.stats.controlRodLevel = reactor.getControlRedLevel(1)
+end
+
+pullReactorInfo(reactor)
+
+while(reactor.stats.state)
+do
 
 
--- Check if a reactor is present
-function isReactor()
-  return component.br_reactor
 end
