584eef90026eaa23a645366ad18879d9f207d896
derrikjb <derrikjb93@gmail.com>
2017-01-28 00:11:13 -0600

    Added main control file

---
+++ control.lua
@@ -0,0 +1,23 @@
+
+/* Main file for reactor control*/
+
+local filesystem = require("filesystem")
+local component = require("component")
+local keyboard = require("keyboard")
+local event = require("event")
+
+local reactor = isReactor()
+
+if (reactor)
+  then
+    print("Error: Computer not connected to Reactor Computer Port")
+  else
+    print("Successful connection to reactor")
+end
+
+
+
+// Check if a reactor is present
+function isReactor()
+  return component.br_reactor
+end
